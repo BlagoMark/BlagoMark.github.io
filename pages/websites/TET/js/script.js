4baa4645094b775291b55537a4f75757bc740600
BlagoMark <47456143+BlagoMark@users.noreply.github.com>
2021-12-23 23:03:16 +0200

    Update script.js

--- pages/websites/TET/js/script.js
+++ pages/websites/TET/js/script.js
@@ -1,3 +1,15 @@
+// Phone number field
+
 $(function(){
   $("#phone").mask("+7 (999) 999 - 99 - 99");
+});
+
+// navbar toggle
+
+const menu_btn = document.querySelector('.burger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
 });
