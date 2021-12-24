88cff26b322307e014ad144f0692557a9230689f
BlagoMark <47456143+BlagoMark@users.noreply.github.com>
2021-12-24 13:44:36 +0200

    Update script.js

--- pages/websites/TET/js/script.js
+++ pages/websites/TET/js/script.js
@@ -13,3 +13,37 @@ menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
   mobile_menu.classList.toggle('is-active');
 });
+
+// slider
+
+$(function(){
+  $('.experts-slider').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
