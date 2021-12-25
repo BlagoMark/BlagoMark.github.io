8b8eda0c0014da927dcc4c49f9e96748874da09d
BlagoMark <47456143+BlagoMark@users.noreply.github.com>
2021-12-25 12:29:11 +0200

    Update script.js

--- pages/websites/TET/js/script.js
+++ pages/websites/TET/js/script.js
@@ -1,7 +1,7 @@
 // Phone number field
 
 $(function(){
-  $("#phone").mask("+7 (999) 999 - 99 - 99");
+  $("#phone").mask("+38 (999) 999 - 99 - 99");
 });
 
 // navbar toggle
@@ -42,6 +42,54 @@ $(function(){
         breakpoint: 500,
         settings: {
           slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.publications-slider').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.trust-slider').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
         }
       }
     ]
