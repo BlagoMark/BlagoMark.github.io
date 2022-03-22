82cd2cfe8d980a894b763cf6f7b612e1584d6069
BlagoMark <47456143+BlagoMark@users.noreply.github.com>
2022-03-22 18:25:44 +0200

    Add files via upload

---
+++ pages/websites/holy-eat/js/script.js
@@ -0,0 +1,51 @@
+// menu
+const burger = document.querySelector(".burger"),
+  menuItem = document.querySelectorAll(".menu__link"),
+  menu = document.querySelector(".header__menu"),
+  overlay = document.querySelector(".overlay");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+// FADEIN
+const fadeIn = (el, timeOut, display) => {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+  el.style.transition = `opacity ${timeOut}ms`;
+  setTimeout(() => {
+    el.style.opacity = 1;
+  }, 10);
+};
+// FADEOUT
+const fadeOut = (el, timeOut) => {
+  el.style.opacity = 1;
+  el.style.transition = `opacity ${timeOut}ms`;
+  el.style.opacity = 0;
+  setTimeout(() => {
+    el.style.display = "none";
+  }, timeOut);
+};
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
